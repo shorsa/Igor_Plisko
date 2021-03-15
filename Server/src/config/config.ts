a2b74772d5adc59c177f9a971ae431410731e431
igorplisko <igorplisko95@gmail.com>
2021-03-15 14:35:14 +0200

    connect

--- Server/src/config/config.ts
+++ Server/src/config/config.ts
@@ -7,8 +7,8 @@ export default {
     PORT: process.env.PORT || "3000",
 
     DB_DIALECT: process.env.DB_DIALECT || "mongo",
-    // DB_HOST: process.env.DB_HOST || "mongodb+srv://example:Example123@cluster0.r7rtf.mongodb.net/test",
-    DB_HOST: process.env.DB_HOST || "mongodb+srv://example:Example123@cluster0.r7rtf.mongodb.net/test",
+    DB_HOST: process.env.DB_HOST || "mongodb://localhost:27017/bd",
+   
     DB_NAME: process.env.DB_NAME || "Example",
     DB_PASSWORD: process.env.DB_PASSWORD || "Example123",
     DB_PORT: process.env.DB_PORT || "27017",
