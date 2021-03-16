e19e835ba8b849f257caf8db08f17906860a6541
igorplisko <igorplisko95@gmail.com>
2021-03-16 14:25:45 +0200

    dev

--- Server/src/features/auth/validation/userRegister.schema.ts
+++ Server/src/features/auth/validation/userRegister.schema.ts
@@ -5,13 +5,13 @@ import { RequestUserModel } from "../models";
 export const userRegisterSchema = object().shape<RequestUserModel>(
    {
       email: string().required().email('Please Enter your Email'),
-      password: string().required('Please Enter your password').min(8),
+      password: string().required('Please Enter your password').min(8).max(15),
       firstName: string().required('Please Enter your first name'),
-      lastName: string().required('Please Enter your last name'),    //!min max
-      phoneNumber: string().required('Please Enter a phone number'),
-      gender: mixed().oneOf(Object.values(Gender) as Gender[]).required('Please Enter a phone number'),    //! не понятно 
+      lastName: string().required('Please Enter your last name'),
+      phoneNumber: string().required('Please Enter a phone number').min(9).max(15),
+      gender: mixed().oneOf(Object.values(Gender) as Gender[]).required('Please Enter a phone number'),
       country: string().required('Please Enter your country'),
-      age: number().required('Please Enter your age'),
+      age: number().required('Please Enter your age').min(1).max(2),
    }
 );
 
