0d9b58758e7749e632ef05d4e516fbfa77900a23
igorplisko <igorplisko95@gmail.com>
2021-03-16 13:31:33 +0200

    dev

--- Server/src/features/auth/auth.service.ts
+++ Server/src/features/auth/auth.service.ts
@@ -1,6 +1,25 @@
+import { findByEmailCount } from "./auth.repository";
 import { Role } from "./enums";
+import { userRegisterSchema } from "./validation/userRegister.schema";
 
-export async function register(body:any) {
+export async function register(body: any) {
    console.log(Role[Role.User]);
-   
+   const a = await userRegisterSchema.isValid(body);
+   if (!a) {
+      throw ("Error")
+   }
+
+   const b = findByEmailCount(body.email)
+
+   //Проверка валидации
+
+
+
+   //Проверка что мыла такого нет в бд
+
+
+
+
+   //Хеширование пароля 
+   //
 }
