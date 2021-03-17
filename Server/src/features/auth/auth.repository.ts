9fe1fb3c38461689b01e746c4aafa20463da3f61
igorplisko <igorplisko95@gmail.com>
2021-03-17 15:41:09 +0200

    dev

--- Server/src/features/auth/auth.repository.ts
+++ Server/src/features/auth/auth.repository.ts
@@ -26,11 +26,30 @@ export async function create(user: RequestUserModel): Promise<ResponseUserModel>
 //! ищем по гендеру
 //? спросить у Влада
 export async function findOfGender(gender: Gender) {
-    const findOfGender = await UserEntityModel.find({ gender: gender })
-    return findOfGender                                                       //?
+    try {
+        const findOfGender = await UserEntityModel.find({ gender: gender })
+        return findOfGender
+
+    } catch (error) {
+        console.log("Error", error)
+        throw (error)
+    }
+    //?
 }
 
-// Delete user
-// export async function deleteUser(id: string) {
 
+
+
+
+// //? сначало нужно получить юзера а потом удалять 
+// //!Delete user
+// export async function deleteUser(id: string) {                                              //?Promise<void>
+//     try {
+//         const repositoryUserDelete = await UserEntityModel;
+//         return repositoryUserDelete;
+
+//     } catch (error) {
+//         console.log("Error", error)
+//         throw (error)
+//     }
 // }
