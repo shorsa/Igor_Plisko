f84680fc017c97c491ed61f0335d5b5c50b97163
igorplisko <igorplisko95@gmail.com>
2021-03-17 12:39:32 +0200

    dev

--- Server/src/features/auth/auth.routes.ts
+++ Server/src/features/auth/auth.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { ApiEndpointsConstants } from "../../config/api-endpoints.constants";
 import { registerHandler } from "./auth.controller";
 
 export const authRouter: Router = Router();
 
-authRouter.post('/register', registerHandler)
-
+authRouter.post(ApiEndpointsConstants.AUTH_REGISTER, registerHandler)
+authRouter.delete('',)
+authRouter.get('',)
+authRouter.post('',)
+                                 //!ApiEndpointsConstants
