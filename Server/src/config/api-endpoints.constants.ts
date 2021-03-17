28f4a3e91ebaf6fb11dfc95ce4354039e85327ce
igorplisko <igorplisko95@gmail.com>
2021-03-17 12:34:44 +0200

    dev

--- Server/src/config/api-endpoints.constants.ts
+++ Server/src/config/api-endpoints.constants.ts
@@ -3,14 +3,15 @@ export class ApiEndpointsConstants {
 
     public static EXAMPLE_FEATURE: string = '/example';
     public static TEST: string = '/test';
-    
+
     public static PROJECTS_FEATURE: string = '/example';
     public static PROJECTS_CREATE: string = '/test';
-    
-    
+
+
     //!Auth путь
     public static AUTH_FEATURE: string = '/auth';
 
+    public static AUTH_REGISTER: string = '/register'
 
 
 }
