d334d9a0fc15c6448025b5a1f83029754ba2da53
igorplisko <igorplisko95@gmail.com>
2021-03-17 12:40:37 +0200

    dev

--- Server/src/features/auth/models/response/responseUser.model.ts
+++ Server/src/features/auth/models/response/responseUser.model.ts
@@ -1,7 +1,7 @@
 import { Gender, Role } from "../../enums";
 
 export interface ResponseUserModel {
-   _id:string;
+   _id: string;
    email: string;
    password: string;
    firstName: string;
@@ -9,9 +9,9 @@ export interface ResponseUserModel {
    phoneNumber: string;
    gender: Gender;
    country: string;
-   age: number;
+   age: Date;
    role: Role;
-   createdAt:Date;
+   createdAt: Date;
 }
 
 //! Респонс у нас всегда идет с "id"
