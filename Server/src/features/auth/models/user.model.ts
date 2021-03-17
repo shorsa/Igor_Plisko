082cb2edad5774ee8b035ceb4fbcf7cb3a60f148
igorplisko <igorplisko95@gmail.com>
2021-03-17 12:34:15 +0200

    dev

--- Server/src/features/auth/models/user.model.ts
+++ Server/src/features/auth/models/user.model.ts
@@ -1,15 +1,15 @@
 import { Gender, Role } from "../enums";
 
 export interface User {
-   email: string;
-   password: string;
-   firstName: string;
-   lastName: string;
-   phoneNumber: string;
-   gender: Gender;
-   country: string;
-   age: number;
-   role: Role;
-   createdAt:Date;
- } 
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  gender: Gender;
+  country: string;
+  age: Date;
+  role: Role;
+  createdAt: Date;
+}
 
