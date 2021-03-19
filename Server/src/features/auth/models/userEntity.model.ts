f42a1e3d71e87282acb5eace21a90bbe7f8d1b9d
igorplisko <igorplisko95@gmail.com>
2021-03-19 14:03:21 +0200

    dev

---
+++ Server/src/features/auth/models/userEntity.model.ts
@@ -0,0 +1,15 @@
+import { Gender, Role } from "../enums";
+
+export interface UserEntityModel {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  gender: Gender;
+  country: string;
+  age: Date;
+  role: Role;
+  createdAt: Date;
+}
+
