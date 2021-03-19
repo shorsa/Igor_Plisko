91ec9cb1d66e0605b048fe0a3325bcf5974a9408
igorplisko <igorplisko95@gmail.com>
2021-03-19 10:43:58 +0200

    DEV

--- Server/src/features/auth/auth.controller.ts
+++ Server/src/features/auth/auth.controller.ts
@@ -9,10 +9,11 @@ export function registerHandler(req: Request, res: Response) {
         .catch((err) => res.send(err));
 }
 
-// export function deleteUser(req: Request, res: Response) {
-//     authService.deleteUser((req.body.id))              //!это не правильно 
-//         .then((result) => {
-//             res.send(result)
-//         })
-//         .catch((err) => res.send(err));
-// }
+export function deleteUser(req: Request, res: Response) {
+
+    authService.deleteUser(req.query.id as string)              //!это не правильно 
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => res.send(err));
+}
