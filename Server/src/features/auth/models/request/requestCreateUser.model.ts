6cbf503f7b9fb1aeb7d0500c18510c86a76ac652
igorplisko <igorplisko95@gmail.com>
2021-03-19 14:06:30 +0200

    dev

---
+++ Server/src/features/auth/models/request/requestCreateUser.model.ts
@@ -0,0 +1,16 @@
+import { Gender } from "../../enums";
+
+export interface RequestCreateUserModel {
+   email: string;
+   password: string;
+   firstName: string;
+   lastName: string;
+   phoneNumber: string;
+   gender: Gender;
+   country: string;
+   age: Date;
+}
+
+
+//!Мы убераем поля    " role: Role;   createdAt:Date;" потому, эти поля для запроса !   
+//!  " role: Role;   createdAt:Date;" еще не известны 
