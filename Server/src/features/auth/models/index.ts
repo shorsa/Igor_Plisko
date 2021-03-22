5afe04d60ceaf95eed9acb3e06bd864ff5239d53
igorplisko <igorplisko95@gmail.com>
2021-03-22 13:55:58 +0200

    dev

--- Server/src/features/auth/models/index.ts
+++ Server/src/features/auth/models/index.ts
@@ -6,5 +6,4 @@ export * from "./response/responseUserRegister.model";
 
 
 export * from "./request/requestCreateUser.model";
-
-//!експортим через индекс! Что бы потом не писать много импортов 
+export * from "./request/requestLoginUser.model"
