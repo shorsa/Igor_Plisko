973d559b826f5f082a683c2e117b273a248c17cd
igorplisko <igorplisko95@gmail.com>
2021-03-22 14:13:43 +0200

    dev

--- Server/src/features/auth/models/index.ts
+++ Server/src/features/auth/models/index.ts
@@ -1,9 +1,9 @@
 export * from "./userEntity.model";
 export * from "./user.model";
-
+//------------------------------------------------------------
 export * from "./response/responseUserRegister.model";
+export * from "./response/responseLoginUser.model"
 
-
-
+//-------------------------------------------------------------
 export * from "./request/requestCreateUser.model";
 export * from "./request/requestLoginUser.model"
