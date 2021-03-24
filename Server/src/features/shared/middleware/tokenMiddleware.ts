e1229fe4ddce90a4b72ee25e919a3d1a07b0396b
igorplisko <igorplisko95@gmail.com>
2021-03-24 15:23:01 +0200

    refactored the middleware token

--- Server/src/features/shared/middleware/tokenMiddleware.ts
+++ Server/src/features/shared/middleware/tokenMiddleware.ts
@@ -5,11 +5,11 @@ import { Request, Response } from "express";
 
 export function verifyTokenMiddleware(req: Request, res: Response, next: any) {
    try {
-      const token = req.headers["authorization"]?.split(' ')[1];
+      const token: string | undefined = req.headers["authorization"]?.split(' ')[1];
       if (!token) {
          return res.status(401).json({ message: "Not authorized" })
       }
-      const decoded = jwt.verify(token, CONFIG.JWT_ENCRYPTION)
+      const decoded: string | object = jwt.verify(token, CONFIG.JWT_ENCRYPTION)
       next()
 
    } catch (err) {
