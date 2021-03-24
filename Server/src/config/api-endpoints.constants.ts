2c3528b1696353e15554263d8f1ff76e9ed9005f
igorplisko <igorplisko95@gmail.com>
2021-03-24 17:44:24 +0200

    refactored the code

--- Server/src/config/api-endpoints.constants.ts
+++ Server/src/config/api-endpoints.constants.ts
@@ -4,18 +4,10 @@ export class ApiEndpointsConstants {
     public static EXAMPLE_FEATURE: string = '/example';
     public static TEST: string = '/test';
 
-
-    //Auth
     public static AUTH_FEATURE: string = '/auth';
     public static AUTH_REGISTER: string = '/register';
     public static AUTH_LOGIN: string = '/login';
 
-
-
-
-    //User
-    public static DELETE_USER: string = '/delete?:id';
-    public static GETTING_USER: string = '/get?:id';
-
+    public static PROJECT: string = '/project';
 
 }
