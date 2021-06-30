38980ed16874d260233b6543e0a3a832022afb90
igorplisko <igorplisko95@gmail.com>
2021-06-30 09:10:58 +0300

    created local storage and token

--- Client/src/shared/helpers/Token.helper.ts
+++ Client/src/shared/helpers/Token.helper.ts
@@ -1,6 +1,21 @@
-import jwt_decode from "jwt-decode"
-import { getAccessToken } from "./LocalStorage.helper"
+// import jwt_decode from "jwt-decode"
+// import { getAccessToken } from "./LocalStorage.helper"
 
-export function getUserId(): any {
-   let
-}
+// export function getUserId(): any {
+//    const token: string | undefined = getAccessToken()
+//    let decoded = jwt_decode(token)
+//    console.log(decoded)
+
+
+//    if (!token) {
+//       return false
+//    }
+
+
+//    else {
+//       let decoded = jwt_decode(token)
+
+
+//    }
+
+// }
